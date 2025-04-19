091d6ef9ae240cfffb09156440f354cf523b0a19
roohasri <rroohar@gmail.com>
2025-04-19 12:50:34 +0530

    Add task2.py

--- files.py
+++ files.py
@@ -1,4 +1,9 @@
-file1 = open('sample.txt', 'r')
-reading_file = file1.read()
-print(reading_file)
-file1.close()
+try:
+    #opening the file and reading
+    file1 = open('sample.txt', 'r')
+    reading_file = file1.read()
+    print(reading_file)
+    file1.close()
+    #if the file is not found error is shown
+except FileNotFoundError: 
+    print("Error: The file was not found. Please check the file path and try again.")
